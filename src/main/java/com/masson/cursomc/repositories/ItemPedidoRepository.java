ebc461050e690a99f1ecb8bea4975614ee6f05bd
gmalves <massonalves@hotmail.com>
2018-09-25 23:10:19 -0300

    ItemPeido e ItemPedidoPK

---
+++ src/main/java/com/masson/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.masson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masson.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+	
+}
