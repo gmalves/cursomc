7c3cfa4e21a2bd89c873543eabd29dbc8ccab261
gmalves <massonalves@hotmail.com>
2018-09-27 21:52:16 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/masson/cursomc/resources/PedidoResource.java
@@ -0,0 +1,32 @@
+package com.masson.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masson.cursomc.domain.Pedido;
+import com.masson.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value= "/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		//PathVariable serve para indicar q o id vindo da requisição será usado como parametro
+		
+		Pedido obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
